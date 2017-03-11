c2981c8d6a4b991c7a979f2073d0a6a101a3b4a4
Sean Darcy <darcys22@gmail.com>
2017-03-11 20:59:03 +1100

    big chunk of file demo made

--- js/main.js
+++ js/main.js
@@ -4,6 +4,234 @@ function convert() {
 function addEmployee() {
 	window.employees.push($('#addEmployee').serializeObject());
   $('#addEmployee')[0].reset();
+  tableCreate();
+}
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+
+function createEmpdupe() {
+  window.empdupe = "";
+  addSupplierDataRecords();
+  addPayerIdentityDataRecord();
+  addSoftwareDataRecord();
+  for (var i = 0; i < window.employees.length; i++) {
+    addPaymentSummaryDataRecord(window.employees[i]);
+  }
+  addFileTotalRecord();
+  download("empdupe", window.empdupe);
+}
+
+function addSupplierDataRecords() {
+  var supplier = { "ABN": "12345678901",
+              "endDate": "30062016",
+              "name": "Sean",
+              "contactName": "Sean",
+              "number": "0414123456",
+              "address": "123 Fake St",
+              "suburb": "Albury",
+              "state": "NSW",
+              "postcode": "2640"
+  }
+
+  //record length and identifier
+  window.empdupe += "628IDENTREGISTER1"
+  //TODO: ABN verifier
+  window.empdupe += supplier.ABN
+  //TODO: Run Type P = Production, T = Test
+  window.empdupe += "T"
+  // ReportEndDate
+  window.empdupe += supplier.endDate
+  //Data Type payg withholding summaries must be E, Type of report must be A, format of report must be P
+  window.empdupe += "EAP"
+  //Report specification number
+  window.empdupe += "FEMPA012.0"
+  //Filler
+  catAlphaNumeric(578, " ");
+  window.empdupe += "\r\n";
+  //record length and identifier
+  window.empdupe += "628IDENTREGISTER2"
+  //SupplierName
+  catAlphanumeric(200, supplier.name);
+  //Supplier Contact Name
+  catAlphanumeric(38, supplier.contactName);
+  //Supplier Number
+  catAlphanumeric(38, supplier.number);
+  //Filler
+  catAlphanumeric(15 + 16 + 327, " ");
+  window.empdupe += "\r\n";
+  //record length and identifier
+  window.empdupe += "628IDENTREGISTER3"
+  //Supplier street address
+  catAlphanumeric(38,supplier.address);
+  catAlphanumeric(38, "");
+  //Supplier suburb
+  catAlphanumeric(27, supplier.suburb);
+  //Supplier state
+  catAlphanumeric(3, supplier.state);
+  //Supplier postcode
+  catNumeric(4, supplier.postcode);
+  //Supplier country (blank for Aus) 
+  catAlphanumeric(20, "");
+  //Supplier postal add 
+  catAlphanumeric(38 + 38 + 27 + 3, "");
+  catNumeric(4,0);
+  catAlphanumeric(20, "");
+  //Supplier email
+  catAlphanumeric(76, "");
+  //Filler
+  catAlphanumeric(275, "");
+  window.empdupe += "\r\n";
+
+}
+
+function addPayerIdentityDataRecord() {
+  var payer = { "ABN": "12345678901",
+              "ABNBranch": "001",
+              "financialYear": "2016",
+              "name": "Sean",
+              "tradingName": "Sean",
+              "contactName": "Sean",
+              "number": "0414123456",
+              "address": "123 Fake St",
+              "suburb": "Albury",
+              "state": "NSW",
+              "postcode": "2640"
+  }
+
+  //record length and identifier
+  window.empdupe += "628IDENTITY"
+  //TODO: ABN Verifier
+  window.empdupe += payer.ABN
+  //ABN Branch Number
+  catNumeric(3,payer.ABNBranch);
+  //Financial Year
+  catNumeric(4,payer.financialYear);
+  //Payer Name
+  catAlphanumeric(200, payer.name);
+  //Payer Trading Name
+  catAlphanumeric(200, payer.tradingName);
+  //Payer street address
+  catAlphanumeric(38, payer.address);
+  catAlphanumeric(38, "");
+  //Payer suburb
+  catAlphanumeric(27, payer.suburb);
+  //Payer state
+  catAlphanumeric(3, payer.state);
+  //Payer postcode
+  catNumeric(4, payer.postcode);
+  //Payer country (blank for Aus) 
+  catAlphanumeric(20, "");
+  //Payer Contact Name
+  catAlphanumeric(38, payer.contactName);
+  //Supplier Number
+  catAlphanumeric(15, payer.number);
+  //Filler
+  catAlphaNumeric(15 + 1, "");
+  window.empdupe += "\r\n";
+}
+
+
+function addSoftwareDataRecord() {
+  //record length and identifier
+  window.empdupe += "628SOFTWARE"
+  //Software product Type
+  catAlphanumeric(80, "COMMERCIAL Sean Darcy DarcyFinancial EmpdupeCreator 1");
+  //TODO: ECI tested
+  window.empdupe += "Y"
+  //Filler
+  catAlphaNumeric(536, "");
+  window.empdupe += "\r\n";
+}
+
+function addPaymentSummaryDataRecord(arrayPosition) {
+  var employee = { "TFN": "12345678901",
+              "DOB": "22111990",
+              "periodStart": "22111990",
+              "periodEnd": "22111990",
+              "name": "Sean",
+              "surname": "Sean",
+              "contactName": "Sean",
+              "number": "0414123456",
+              "address": "123 Fake St",
+              "suburb": "Albury",
+              "state": "NSW",
+              "postcode": "2640"
+  }
+
+  //record length and identifier and Income type S for salary
+  window.empdupe += "628DINBS"
+  //TODO: TFN Valiadator
+  catNumeric(9,employee.TFN);
+  //DOB Year
+  catDate(employee.DOB);
+  //Employee Surname
+  catAlphanumeric(30, employee.surname);
+  //Employee Name
+  catAlphanumeric(15, employee.name);
+  //Payer Trading Name
+  catAlphanumeric(200, payer.tradingName);
+  //Payer street address
+  catAlphanumeric(38, payer.address);
+  catAlphanumeric(38, "");
+  //Payer suburb
+  catAlphanumeric(27, payer.suburb);
+  //Payer state
+  catAlphanumeric(3, payer.state);
+  //Payer postcode
+  catNumeric(4, payer.postcode);
+  //Payer country (blank for Aus) 
+  catAlphanumeric(20, "");
+  //Payer Contact Name
+  catAlphanumeric(38, payer.contactName);
+  //Supplier Number
+  catAlphanumeric(15, payer.number);
+  //Filler
+  catAlphaNumeric(15 + 1, "");
+  window.empdupe += "\r\n";
+}
+
+function catAlphaNumeric(length, text) {
+  window.empdupe += padding_right(text, " ", length)
+}
+function catDate(date) {
+  window.empdupe += "30062016"
+}
+function catNumeric(length, num) {
+  var n = String(num); 
+  window.empdupe += padding_left(n, "0", length)
+}
+// left padding s with c to a total of n chars
+function padding_left(s, c, n) {
+  if (! s || ! c || s.length >= n) {
+    return s;
+  }
+  var max = (n - s.length)/c.length;
+  for (var i = 0; i < max; i++) {
+    s = c + s;
+  }
+  return s;
+}
+// right padding s with c to a total of n chars
+function padding_right(s, c, n) {
+  if (! s || ! c || s.length >= n) {
+    return s;
+  }
+  var max = (n - s.length)/c.length;
+  for (var i = 0; i < max; i++) {
+    s += c;
+  }
+  return s;
 }
 
 function editPayer() {
@@ -26,6 +254,56 @@ $.fn.serializeObject = function()
     });
     return o;
 };
+
+function checkABN(str) {
+    if (!str || str.length !== 11) {
+        return false;
+    }
+    var weights = [10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19],
+        checksum = str.split('').map(Number).reduce(
+        function(total, digit, index) {
+            if (!index) {
+                digit--;
+            }
+            return total + (digit * weights[index]);
+        },
+        0
+    );
+
+    if (!checksum || checksum % 89 !== 0) {
+        return false;
+    }
+
+    return true;
+}
+
+function tableCreate() {
+    var tbdy = document.getElementById('employeetable');
+    tbdy.innerHTML = '';
+    for (var i = 0; i < window.employees.length; i++) {
+        var tr = document.createElement('tr');
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode(window.employees[i].surname + ', ' +window.employees[i].first_name))
+        tr.appendChild(td)
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode(window.employees[i].tfn))
+        tr.appendChild(td)
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode(window.employees[i].gross_payments))
+        tr.appendChild(td)
+        var td = document.createElement('td');
+        td.appendChild(document.createTextNode(window.employees[i].tax_withheld))
+        tr.appendChild(td)
+        var td = document.createElement('td');
+        var btn = document.createElement('button');
+        btn.className = 'btn btn-danger';
+        btn.innerHTML = "-";
+        td.appendChild(btn)
+        tr.appendChild(td)
+        tbdy.appendChild(tr);
+    }
+}
+
 
 function main() {
   window.employees = [];
